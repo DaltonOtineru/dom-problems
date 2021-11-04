5b2e21186dc70cf83cdba18cb6367b881765a256
Dalton Otineru <d@dmbp.local>
2021-11-03 17:27:37 -0700

    dom problem 3

--- problem-3/script.js
+++ problem-3/script.js
@@ -5,7 +5,7 @@ function newRow {
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    headerCell.innerHTML = "Difficulty";
+    cell1.innerHTML = "Difficulty";
     cell2.innerHTML = "Easiest";
     cell3.innerHTML = "Harder";
     cell4.innerHTML = "Hardest";
