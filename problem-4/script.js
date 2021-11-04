512eaf8ca339344423cf6a001fa7b0cca7ec533a
Dalton Otineru <d@dmbp.local>
2021-11-04 14:41:47 -0700

    fixed problem #4

--- problem-4/script.js
+++ problem-4/script.js
@@ -1,18 +1,10 @@
 function dom() {
-    const list = document.getElementById("music");
-    const listLength = list.length;
-    console.log(listLength);
-    const option1 = list.options[list.selectedIndex = "0"].value;
-    //console.log(listItem1);
-    const option2 = list.options[list.selectedIndex = "1"].value;
-    //console.log(listItem2);
-    const option3 = list.options[list.selectedIndex = "2"].value;
-    //console.log(listItem3);
-    const options = [option1, option2, option3];
-    //alert(listLength, option1, option2, option3);
-    const optionString = options.toString();
-    alert(optionString + " " + listLength);
+    const list = document.getElementById("music").options;
+    const option1 = list[0].value;
+    const option2 = list[1].value;
+    const option3 = list[2].value;
+    alert(`The music options are ${option1}, ${option2}, ${option3}. There are ${list.length} options!`);
+ }
+   
     
-    console.log(options);
-}
-dom();
+
